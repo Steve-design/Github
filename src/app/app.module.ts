ad71dc12a96f30493a81544f9a071a5603802ec4
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-28 12:07:12 +0300

    create search form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
