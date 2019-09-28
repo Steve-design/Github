64befac19a42da6409bda7eab8abf79cfe067366
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-28 14:13:31 +0300

    import RepoRequestService to the component

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RepoRequestService } from './repo-request.service';
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
