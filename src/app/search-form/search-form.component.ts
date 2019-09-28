3e0bc0d81e40e79aca0ab52ca1b90d88381fac13
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-28 12:16:20 +0300

    add a serch function to the the form

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SearchFormComponent implements OnInit {
 
+  @Output() emitSearch = new EventEmitter<any>() 
+
+
+  searchTerm: string;
   constructor() { }
+
+
+  search(){
+    this.emitSearch.emit(this.searchTerm)
+  }
 
   ngOnInit() {
   }
