c538e0345de23b2c3a3d67ae62909a65365ebdeb
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-28 14:35:43 +0300

    add a redirect path to the landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { LandingComponent } from './landing/landing.component';
+import { RepositoryComponent } from './repository/repository.component';
+const routes: Routes = [
+{ path: "landing", component:LandingComponent },
+{ path: "repository", component:RepositoryComponent},
+{ path: '', redirectTo:"/landing", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
