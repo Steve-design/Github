614cfa43c7caaf98c7624d16b146a85475edf574
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-28 14:50:57 +0300

    add functionality to so as to search and find repositories

--- src/app/repo-http/repo-request.service.ts
+++ src/app/repo-http/repo-request.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment"
+import { Repository } from './repository';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoRequestService {
+  gitrepos: Repository;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  searchRepo(searchTerm:string){
+    interface  Results{
+      
+       name: string,
+        description: string, 
+        html_url: string,
+        url:string 
+        
+    }
+
+    let searchEndpoint= "https://api.github.com/users/"+searchTerm+"/repos?access_token="+environment.GITHUB_API;
+
+    let promise = new Promise((resolve, reject)=>{
+      this.http.get<Results>(searchEndpoint).toPromise().then(
+        (results)=>{
+           this.gitrepos = results;
+          
+          console.log(this.gitrepos)
+          resolve()
+        },
+        (error)=>{
+          console.log(error);
+          reject()
+        }
+      )
+    })
+    return promise;
+  }
 }
