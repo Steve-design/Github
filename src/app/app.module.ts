75cd085f8287b5e8a05d903aefe9def41697c6ce
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-28 13:43:24 +0300

    create date pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     SearchFormComponent,
-    RepositoryComponent
+    RepositoryComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
