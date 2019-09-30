1e69ad10fd03af0ff42ef736046aebdaef579d92
Nancy Muthinzi <nanciekathini@gmail.com>
2019-09-30 08:09:06 +0300

    import user and repo, add functionality to landing script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { RepositoryComponent } from './repository/repository.component';
 import { DatePipe } from './date.pipe';
+import { HttpClientModule } from "@angular/common/http";
 import { NavbarComponent } from './navbar/navbar.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +20,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserRequestService} from "../user-http/user-request.service";
+import { User } from "../user";
+import { Repository } from '../repository';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public UserRequestService:UserRequestService)  { }
+  
+  searchUser(searchTerm){
+    this.UserRequestService.searchUser(searchTerm).then(
+      (results)=>{
+        this.user = this.UserRequestService.user;
+    
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+  }
+
 
   ngOnInit() {
+    this.searchUser("Steve-design")
   }
+
 
 }
